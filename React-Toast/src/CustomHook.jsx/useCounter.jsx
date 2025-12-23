e04ed5329d8c7e9b7e737127c4422d11fc2ac5dc
Yogesh Sharma <webdeveloper.yogesh63@gmail.com>
2025-12-23 20:26:33 +0530

    #Commit-Redundand code removal

--- React-Toast/src/CustomHook.jsx/useCounter.jsx
+++ React-Toast/src/CustomHook.jsx/useCounter.jsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
 
 function useCounter(initialVal= 0,step=1){
-
     const [count,setCount]= useState(initialVal)
 
     const increment= ()=>{
-   setCount((prev)=> prev+ step)
+    setCount((prev)=> prev+ step)
     }
     const decrement= ()=>{
-         setCount((prev)=> prev- step)
+    setCount((prev)=> prev- step)
     }
     const reset= ()=>{
-            setCount(initialVal)
+    setCount(initialVal)
     }
-
     return {count,increment,decrement,reset}
 }
 
